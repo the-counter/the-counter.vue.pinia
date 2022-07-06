6f4c600ba30e6e108b0f34755eb5ceff43e3abdb
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-07-06 09:36:50 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import CounterForm from './components/CounterForm.vue';
+</script>
+
+<template>
+  <Navbar brand="The Counter" />
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <CounterForm />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/CounterForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const decrement = () => {
+  count.value = count.value - 1;
+};
+
+const increment = () => {
+  count.value = count.value + 1;
+};
+</script>
+
+<template>
+  <div class="d-flex justify-content-between align-items-center mt-3">
+    <button
+      class="btn btn-outline-primary"
+      @click="decrement"
+    >-</button>
+    {{ count }}
+    <button
+      class="btn btn-outline-primary"
+      @click="increment"
+    >+</button>
+  </div>
+</template>
